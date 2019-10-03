721631c8c99b00b2b5ac6942f5c46d70009d8a34
transentropy <49517570+transentropy@users.noreply.github.com>
2019-10-04 00:49:12 +0800

    Update VCEP.py

--- VCEP.py
+++ VCEP.py
@@ -2,11 +2,11 @@
 """
 @name: VOCALOID™ of Chinese Moegirlpedia™ Editor Plus
 @author: Transentropy
-@version: Beta 1.2
+@version: Beta 2.0
 @source: https://github.com/transentropy/VCEP
 """
 
-import json
+from json import loads
 import re
 import time
 import urllib.request
@@ -18,25 +18,31 @@ url = 'http(s)?:\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+
 aid = 'av[0-9]+'
 
 vLis = ['洛天依', '言和', '心华', '乐正绫', '星尘',
-        '乐正龙牙', '初音未来', '墨清弦', '徵羽摩柯']
+        '乐正龙牙', '初音未来', '墨清弦', '徵羽摩柯',
+        '赤羽', '诗岸', '苍穹', '海伊']
+
 vDic = {'洛':0, '依':0, '南':0, '言':1, '心':2, '绫':3, '北':3,
         '星':4, '尘':4, '龙':5, '牙':5, '初':6, '葱':6, '墨':7,
-        '弦':7, '摩':8, '柯':8}
+        '弦':7, '摩':8, '柯':8,'赤':9, '羽':9, '诗':10, '岸':10, '苍':11, '穹':11, '海':12}
+
+engineLis = ['VOCALOID', 'Synthesizer V']
 
 divide = {"op":"",
           "ed":"",
           "ch":[],
-          "hh":['/', '&'],
+          "hh":['/', '&', '／'],
           'hc':[':', '：'],
-          'cc':['/', '、', '&']
+          'cc':['/', '、', '&', '／']
           }
+
+forbid = [["http", "简介补充", '作品类型', '.*本家', '原[唱|作]', '.*码'], ["http", "av", "www", "^[0-9]+$"]]
 
 jobList = ['策划', '作曲', '编曲', '作词', '调教', '混音', '母带', '人设', '曲绘',
        '封面', 'PV', '题字', 'LOGO', '美工', '列表外其他','协力', '宣传', '特别感谢', '演唱', '出品']
 
 jobDictZH = {'作':{'曲':1, '词':3, 'default':1},
            '曲':{'作':1, '绘':8, 'default':1},
-           '编':{'曲':2, 'default':2},
+           '编':{'曲':2, '写': '+编写','default':2},
            '词':{'作':3, 'default':3},
            '调':{'音':4, '教':4, '校':4, 'default':4},
            '混':{'音':5, 'default':5},
@@ -47,7 +53,7 @@ jobDictZH = {'作':{'曲':1, '词':3, 'default':1},
            '影':{'default': 10},
            '映':{'像':10, '画':10, 'default':10},
            '唱':{'default': -2},
-           '歌':{'手': -2, '者': -2, 'default': -2},
+           '歌':{'手': -2, '者': -2, '词':3, 'default': -2},
            '协':{'力':-5, '助':-5, 'default':-5},
            '策':{'划':0, 'default':0},
            '呗':{'default':-2},
@@ -59,6 +65,8 @@ jobDictZH = {'作':{'曲':1, '词':3, 'default':1},
            '宣':{'传':-4, 'default':-4},
            '题':{'字':11, 'default':'+'},
            '美':{'工':13, 'default':'+'},
+           '总':'-',
+           '重':'-',
            'default':''
            }
 
@@ -81,18 +89,18 @@ jobDictEN = {'pv':10,
 pageFormat = """
 {{{{Vocaloid_Songbox
 |image = {title}.jpg
-|图片信息 = 曲绘 by
+|图片信息 = 曲绘 by {illu}
 |颜色 = 
 |演唱 = {singers1}
 |歌曲名称 = {title}
-|P主 = [[{up}]]
+|UP主 = [[{up}]]
 |bb_id = av{aid}
 |投稿时间 = {date}{extra}
 |再生 = {{{{bilibiliCount|id={aid}}}}}
 }}}}
 
 == 简介 ==
-《'''{title}'''》是[[{up}]]于{date}投稿至[[bilibili]]的[[Vocaloid]]中文{origin}歌曲，由{singers2}演唱{series}{album}。截至现在已有{{{{bilibiliCount|id={aid}}}}}次观看，{{{{bilibiliCount|id={aid}|type=4}}}}人收藏。
+《'''{title}'''》是[[{up}]]{upjob}于{date}投稿至[[bilibili]]的{engine}中文{origin}歌曲，由{singers2}演唱{series}{album}。截至现在已有{{{{bilibiliCount|id={aid}}}}}次观看，{{{{bilibiliCount|id={aid}|type=4}}}}人收藏。
 
 == 歌曲 ==
 {{{{BilibiliVideo|id={aid}}}}}
@@ -109,7 +117,6 @@ class Intro:
     def __init__(self, text, divide):
         self.text = text
         self.divide = divide
-        self.forbid = [["^http", "简介补充", "曲目类型", '本家$', '^原'], ["^http", "^av", "^www"]]
         self.pattern = re.compile(self.partternMaker())
         self.urls = []
         self.aids = []
@@ -117,7 +124,8 @@ class Intro:
         self.stfLi = self.extract(self.pre(self.text))
         self.autoCheck()
         self.autoParse()
-        self.singers = self.singerCollect()
+        self.singers = self.jobSearcher('演唱')
+        self.illu = self.jobSearcher('曲绘')
         
     def insert(self, index, obj):
         self.stfLi.insert(index, obj)
@@ -188,19 +196,19 @@ class Intro:
     def autoCheck(self):
         index = 0
         while index < len(self.stfLi):
-            forbid = False
+            forbidden = False
             for job in self.stfLi[index][0]:
-                for pat in self.forbid[0]:
+                for pat in forbid[0]:
                     if re.match(pat, job):
-                        forbid = True; break
+                        forbidden = True; break
 
             else:
                 for name in self.stfLi[index][1]:
-                    for pat in self.forbid[1]:
+                    for pat in forbid[1]:
                         if re.match(pat, name):
-                            forbid = True; break
+                            forbidden = True; break
             
-            if forbid:
+            if forbidden:
                 self.remove(index)
                 continue
             index = index + 1
@@ -287,6 +295,21 @@ class Intro:
                         tmpZh = tmpZh + jobStr[index]
                         index = index + 1
                         dic = jobDictZH
+
+                elif path[0] == '+':
+                    extraList[-1] = extraList[-1] + path[1:]
+                    index = index + 1
+                    dic = jobDictZH
+
+                elif path[0] == '-':
+                    index = index + 1
+                    dic = jobDictZH
+
+                else:
+                    extraList.append(path)
+                    index = index + 1
+                    dic = jobDictZH
+                    
         
         path = dic['default']
         if type(path) == int:
@@ -306,23 +329,33 @@ class Intro:
         finalList.extend(extraList)
         return [sortIndex, finalList]
     
-    def singerCollect(self):
+    def jobSearcher(self, job):
         for row in self.stfLi:
-            if '演唱' in row[0]:
+            if job in row[0]:
                 return row[1]
         else:
             return []
-                        
+        
+    def staffSearcher(self, name):
+        jobLis = []
+        for row in self.stfLi:
+            for stf in row[1]:
+                if stf.find(name) != -1:
+                    jobLis.extend(row[0])
+        return jobLis
+        
     def compose(self):
-        if len(self.stfLi) == 0 :
-            return "未提取到有效信息"
         para = """{{Vocaloid_Songbox_Introduction
 |lbgcolor = 
 |ltcolor = #FFFFFF"""
         index = 0
+        tmpLis = []
         while index < len(self.stfLi):
+            tmpLis.clear()
+            for name in self.stfLi[index][1]:
+                tmpLis.append(innerChecker(name))
             para = para + "\n|group" + str(index + 1) + " = " + "<br />".join(self.stfLi[index][0])\
-            + "\n|list" + str(index + 1) + " = " + "<br />".join(self.stfLi[index][1])
+            + "\n|list" + str(index + 1) + " = " + "<br />".join(tmpLis)
             index = index + 1
         para = para + "\n}}"
         return para
@@ -335,7 +368,7 @@ def delString (text, a, b):#del a to b-1, a >= b return original string
 
 class Song:
     def __init__(self, data):
-        self.aid = data['aid']
+        self.aid = str(data['aid'])
         self.oriTitle = data['title']
         self.cover = data['pic']
         self.pubdate = data['pubdate'] + 28800 #UTC+8
@@ -351,32 +384,31 @@ class Song:
             self.singers = self.titleInfo['singers']
             self.introInfo.singers = self.singers
             self.introInfo.appand([['演唱'], self.singers, 14])
-        
+
+        self.engine = self.titleInfo['engine']
+         
     def getFormatTime(self, fm):
         if fm == 0:
             return time.strftime("%Y-%m-%d %H:%M", time.gmtime(self.pubdate))
         else:
-            return time.strftime("%Y{y}%m{m}%d{d}",time.gmtime(self.pubdate))\
-                                 .format(y='年', m='月', d='日')
+            gm = time.gmtime(self.pubdate)
+            return str(gm.tm_year) + '年' + str(gm.tm_mon) + '月' + str(gm.tm_mday) + '日'
     
     @classmethod
     def singerParser(cls, text):
-        vDic = {'洛':0, '依':0, '南':0, '言':1, '心':2, '绫':3, '北':3,
-                '星':4, '尘':4, '龙':5, '牙':5, '初':6, '葱':6, '墨':7,
-                '弦':7, '摩':8, '柯':8}
         singerSet = set()
         for char in text:
-            tmp = vDic.get(char, None)
-            if tmp != None:
-                singerSet.add(tmp)
+            i = vDic.get(char, None)
+            if i != None:
+                singerSet.add(i)
         
         return singerSet
     
     @classmethod                             
     def titleParser(cls, text):
-        infoDict = {'title':'', 'origin':1, 'singers':[],
+        infoDict = {'title':'', 'origin':1, 'singers':[], 'engine':[],
                     'series':'', 'album':'', 'other':[]}
-        symbols = {'op':'【[（(', 'clz':'】]）)', 'top':'《', 'tclz':'》',
+        symbols = {'op':'【[（(『', 'clz':'】]）)』', 'top':'《', 'tclz':'》',
                    'divide':'/，,+&︱、×'}
         tmp = ''
         tmpTitles = []
@@ -414,6 +446,7 @@ class Song:
                 infoDict['other'].append(tmp)
                 
         singerSet = set()
+        engineSet = set()
         
         if len(tmpTitles) > 0:
             tmpLi = re.split("[—-]+|[Ff]eat.|by", tmpTitles[0])
@@ -438,22 +471,33 @@ class Song:
             or item.find("填词") != -1\
             or item.lower().find("cover") != -1:
                 infoDict['origin'] = 0
+                continue
             series = re.search(".+(系列|[p|P]roject)", item)
             if series:
                 infoDict['series'] = series.group().strip(" ")
+                continue
             album = re.search('[《『].+[》』]', item)
             if album:
                 infoDict['album'] = album.group().lstrip('《『').rstrip('》』')
+                continue
             else:
                 album = re.search("(?<=辑).+(?=收录)", item)
                 if album:
                     infoDict['album'] = album.group()
+                    continue
             tmpSet = cls.singerParser(item)
             if len(tmpSet) > len(singerSet):
                 singerSet = tmpSet.copy()
                 
         for i in singerSet:
+            if i <= 8:
+                engineSet.add(engineLis[0])
+            else:
+                engineSet.add(engineLis[1])
+                
             infoDict['singers'].append(vLis[i])
+            
+        infoDict['engine'] = list(engineSet)
         
         return infoDict
     
@@ -467,13 +511,16 @@ class Song:
             extra = ''
         series = self.titleInfo['series']
         if series != '':
-            series = '，为' + series + '第 作'
+            series = '，为' + series + '第■作'
         text = pageFormat.format(title=self.titleInfo['title'],
                                  aid=self.aid,
                                  up=self.uploader,
+                                 upjob = multiJoin(self.introInfo.staffSearcher(self.uploader), '', '、', '、', '并'),
+                                 illu = '、'.join(self.introInfo.illu),
                                  date=self.getFormatTime(1),
                                  singers1="[[" + "]]、[[".join(self.singers) + "]]",
                                  singers2 = multiJoin(self.singers, "[[", ']]、[[', ']]与[[', ']]'),
+                                 engine = multiJoin(self.engine, "[[", ']]、[[', ']]和[[', ']]'),
                                  origin=origin,
                                  album=album,
                                  series=series,
@@ -482,11 +529,23 @@ class Song:
         for s in self.singers:
             text = text + ('\n{{' + s + '|collapsed}}')
         
-        text = text + ('\n[[Category:中国音乐作品]]\n[[Category:使用VOCALOID的歌曲]]')
+        text = text + ('\n[[分类:中国音乐作品]]')
+        for e in self.engine:
+            text = text + ('\n[[分类:使用'+ e +'的歌曲]]')
         for s in self.singers:
             text = text + ('\n[[分类:' + s + '歌曲]]')
             
         return text
+
+
+    
+def innerChecker(name):
+    nameSet = {'洛天依', '言和', '心华', '乐正绫', '星尘',
+    '乐正龙牙', '初音未来', '墨清弦', '徵羽摩柯', '赤羽', '诗岸', '苍穹', '海伊'};
+    if name in nameSet:
+        return '[[' + name + ']]'
+    else:
+        return name
     
 def multiJoin(ls, start, join, last, close):
     re = ""
@@ -507,30 +566,35 @@ def getData(bbid):
     return response.read().decode('utf-8')    
 
 if __name__=="__main__":
-    print ("""VOCALOID™ of Chinese Moegirlpedia™ Editor Plus (Beta 1.0)
+    print ("""VOCALOID™ of Chinese Moegirlpedia™ Editor Plus (Beta 2.0)
 Power by Transentropy©
 GET UPDATED: github.com/transentropy/VCEP\n""")
-    
-    while (1):
-        print("正在连接到神经网络...")
+
+    i = 4
+    while (i > 0):
+        print("正在检测网络连接...")
     #初始化检查网络信息
-        testIof = json.loads(getData("6009789"))
+        testIof = loads(getData("6009789"))
         if testIof['code'] == -1:
-            input("连接失败，请按任意键重连...\n")
-            continue
+            input("Error：访问站点失败，请按任意键重试\n")
+            i = i - 1
         else:
-            print("已成功连接到神经网络")
+            print("已成功连接到网络")
             break
+    else:
+        print("无法连接到网络，请检查无误后重新启动")
+        exit()
     
     file = open("page.txt", 'a')
     while (1):
-        bbid = input("\n请输入AV号，按回车确定；直接回车退出程序\n")
-        if bbid == '':
+        raw = input("\n请输入含有AV号的文本，按回车确定；直接回车退出程序\n")
+        if raw == '':
             break
-        if bbid.isnumeric() == False:
-            print("非法的输入")
-            continue
-        videoInfo = json.loads(getData(bbid))
+        raw = re.search('[0-9]+', raw)
+        if type(raw) == None:
+            print("Error -100：无法识别的输入")
+            continue    
+        videoInfo = loads(getData(raw.group()))
         if videoInfo['code'] != 0:
             print ('Error ' + str(videoInfo['code']) + '：' + videoInfo['message'])
             continue
@@ -546,4 +610,4 @@ GET UPDATED: github.com/transentropy/VCEP\n""")
         file.flush()
         
     file.close()
-   
+    
